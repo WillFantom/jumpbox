511c54bb6e45c0aa72a497cc84f3abfc719a3399
Will Fantom <willf@ntom.dev>
2022-11-16 16:58:27 +0000

    fix: Sanitize the key lookup filter
    Signed-off-by: Will Fantom <willf@ntom.dev>

--- sshd/authorized_keys.sh
+++ sshd/authorized_keys.sh
@@ -1,5 +1,13 @@
 #!/bin/ash
 
+# validate the username, sanitizing the lookup next
+regex="^[a-z_]([a-z0-9_-]{0,31}|[a-z0-9_-]{0,30}\$)$"
+if [[ ! $1 =~ ${regex} ]]; then
+  exit 1
+fi
+
+# find keys for given user
 jq -r -c .${1}'[]' /etc/ssh/keys.d/authorized_keys.json | while read k; do
   echo "$k"
+  exit 0
 done
