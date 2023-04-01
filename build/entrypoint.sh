d8b8642d06974abfc5ee80b23ec26e7fef3317b9
Will Fantom <willf@ntom.dev>
2023-04-01 10:37:41 +0100

    chore: Cleaned up entrypoint script 🧹

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -1,5 +1,10 @@
-#!/bin/ash
-set -e
+#!/usr/bin/env bash
+set -o errexit
+set -o nounset
+set -o pipefail
+if [[ "${TRACE-0}" == "1" ]]; then
+  set -o xtrace
+fi
 
 echo "Setting up host keys..."
 hostkeys_dir=/etc/ssh/hostkeys.d
@@ -17,10 +22,10 @@ echo "Setting up users..."
 keys_dir=/etc/ssh/keys.d
 mkdir -p ${keys_dir}
 echo "Creating users..."
-jq -r 'keys_unsorted[]' ${keys_dir}/authorized_keys.json | while read u; do
+jq -r 'keys_unsorted[]' ${keys_dir}/authorized_keys.json | while read -r u; do
   echo "Creating user ${u}"
   adduser -D -H -s /sbin/nologin "${u}"
-  sed -i s/${u}:!/"${u}:*"/g /etc/shadow
+  sed -i s/"${u}:!"/"${u}:*"/g /etc/shadow
 done
 
 echo "Creating sshd configuration..."
@@ -29,8 +34,8 @@ mv /etc/ssh/sshd_config_envsubst /etc/ssh/sshd_config
 
 if [[ ${ENDLESSH_PORT} -ne "0" ]]; then
   echo "Running endlessh server..."
-  /usr/local/bin/endlessh -p ${ENDLESSH_PORT} -v &
+  /usr/local/bin/endlessh -p "${ENDLESSH_PORT}" -v &
 fi
 
 echo "Running SSH server..."
-/usr/sbin/sshd -D -f /etc/ssh/sshd_config -e -p ${SSHD_PORT}
+/usr/sbin/sshd -D -f /etc/ssh/sshd_config -e -p "${SSHD_PORT}"
