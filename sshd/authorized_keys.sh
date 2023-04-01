7009299b7f5b5292d5d9a7c55bf4088ff6cce5fe
Will Fantom <willf@ntom.dev>
2023-04-01 10:39:04 +0100

    chore: Cleaned up authorized_keys script 🧹

--- sshd/authorized_keys.sh
+++ sshd/authorized_keys.sh
@@ -1,4 +1,10 @@
-#!/bin/ash
+#!/usr/bin/env bash
+set -o errexit
+set -o nounset
+set -o pipefail
+if [[ "${TRACE-0}" == "1" ]]; then
+  set -o xtrace
+fi
 
 # validate the username, sanitizing the lookup next
 regex="^[a-z_]([a-z0-9_-]{0,31}|[a-z0-9_-]{0,30}\$)$"
@@ -7,7 +13,7 @@ if [[ ! $1 =~ ${regex} ]]; then
 fi
 
 # find keys for given user
-jq -r -c .${1}'[]' /etc/ssh/keys.d/authorized_keys.json | while read k; do
+jq -r -c ."${1}"'[]' /etc/ssh/keys.d/authorized_keys.json | while read -r k; do
   echo "$k"
   exit 0
 done
